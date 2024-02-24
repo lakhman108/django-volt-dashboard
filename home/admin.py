847e435cf69c194df5b3d56a4b403ac026dc6040
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2024-02-24 06:41:07 +0000

    Pending changes exported from your codespace

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from home.models import Student
 
-# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'age', 'email')
+
+admin.site.register(Student, StudentAdmin)
